aa76ec9e85cea8426e815c7d7cfda27a97b92778
Min Pilgi <pilboy97@gmail.com>
2024-10-13 23:53:41 +0900

    use viper to set environment variable

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	viper.SetConfigFile(".env")
+	viper.ReadInConfig()
+
+	port := viper.GetString("PORT")
+
+	fmt.Println("Server started at port " + port + "...")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Hi!</h1>")
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
 }
